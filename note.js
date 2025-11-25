a74a29c6be8c8bd426c6b8d10c6ff1e62fb6bf06
Tuấn Anh Trần <alex.anhtt@gmail.com>
2025-11-25 21:15:46 +0700

    Modify note with styled link in last entry
    Updated the last note to include a styled link.

--- note.js
+++ note.js
@@ -1,6 +1,7 @@
 window.notes = [
   [`24.11.2025`, `ngonn.. chạy đc roii:))<br>Hello World!`],
   [`24.11.2025 [22:45]`, `vc quên mất đt k có tính năng hơ chuột méeeeee<br>th ke di!`],
-  [`25.11.2025 [21:10]`, `ai luyện lis vs t k:)) <a href="https://e-learning.youpass.vn/practice/listening/7369">link này</a>`]
+  [`25.11.2025 [21:10]`, `ai luyện lis vs t k:))<br><a href="https://e-learning.youpass.vn/practice/listening/7369" style="color:blue"><b>link này</b></a>`]
 ];
+
 
