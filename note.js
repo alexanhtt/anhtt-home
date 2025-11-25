907fa2971e14b6b01326104b88defc9bae51d0e0
Tuấn Anh Trần <alex.anhtt@gmail.com>
2025-11-25 21:10:20 +0700

    Add new note entry for 25.11.2025

--- note.js
+++ note.js
@@ -1,4 +1,6 @@
 window.notes = [
   [`24.11.2025`, `ngonn.. chạy đc roii:))<br>Hello World!`],
-  [`24.11.2025 [22:45]`, `vc quên mất đt k có tính năng hơ chuột méeeeee<br>th ke di!`]
+  [`24.11.2025 [22:45]`, `vc quên mất đt k có tính năng hơ chuột méeeeee<br>th ke di!`],
+  [`25.11.2025 [21:10]`, `ai luyện lis vs t k:)) <a href="https://e-learning.youpass.vn/practice/listening/7369">link này</a>`]
 ];
+
