eb65d56b32a69dec58299db50db7d66ed457c81b
Tuấn Anh Trần <alex.anhtt@gmail.com>
2025-11-26 00:21:05 +0700

    Update notes with new entries and remove old ones

--- note.js
+++ note.js
@@ -1,7 +1,4 @@
 window.notes = [
   [`24.11.2025`, `ngonn.. chạy đc roii:))<br>Hello World!`],
-  [`24.11.2025 [22:45]`, `vc quên mất đt k có tính năng hơ chuột méeeeee<br>th ke di!`],
-  [`25.11.2025 [21:10]`, `ai luyện lis vs t k:))<br><a href="https://e-learning.youpass.vn/practice/listening/7369" style="color:blue"><b>link này</b></a>`]
+  [`26.11.2025 [00:18]`, `giao diện trên mtinh ok hơn đt:v`],
 ];
-
-
