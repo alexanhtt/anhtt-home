9356b9d4786d6d532b7d198d54ac436aa1bf9e77
Tuấn Anh Trần <alex.anhtt@gmail.com>
2025-12-03 00:29:57 +0700

    Add new note entry for December 3, 2025

--- note.js
+++ note.js
@@ -2,4 +2,5 @@ window.notes = [
   [`24.11.2025`, `ngonn.. chạy đc roii:))<br>Hello World!`],
   [`26.11.2025 [00:18]`, `giao diện trên mtinh ok hơn đt:v`],
   [`30.11.2025 [00:00]`, `oh goshh sắp sang t12 r..<p style="font-size:8px">(ý là sắp thi ie)</p><br><img src="https://i.postimg.cc/8C0WR4MQ/image.png" height="100px" width="175px" title="lis:(("><img src="https://i.postimg.cc/qM2vQwfs/image.png" height="70px" width="100px" title="spe:vv"><h1>đái tht sự!!<h1>`],
+  [`3.12.2025 [00:21]`, `ok<br><img src="https://i.postimg.cc/DzSVyj9N/image.png" height="auto" width="175px"><img src="https://i.postimg.cc/d13b3WgR/image.png" height="auto" width="175px" title="cố gắng 6+">`],
 ];
